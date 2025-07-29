31a9a7e2cbdbb6d4ef8117fafdffb82034ecdff6
ISABELLY LIMA DA SILVA <24122387@dsn127m1242316.cfp127aluno.local>
2025-07-29 17:24:58 -0300

    delete por id ok

--- app/src/main/java/br/senai/sp/jandira/clientesappdsa/service/ClienteService.kt
+++ app/src/main/java/br/senai/sp/jandira/clientesappdsa/service/ClienteService.kt
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.clientesappdsa.service
 import br.senai.sp.jandira.clientesappdsa.model.Cliente
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -17,5 +18,8 @@ interface ClienteService {
 
     @GET("clientes/{id}")
     fun buscarPorId(@Path("id") codigo: Long): Call<List<Cliente>>
+
+    @DELETE("clientes/{id}")
+    fun deletarPorId(@Path("id") codigo: Long): Call<List<Cliente>>
 
 }
