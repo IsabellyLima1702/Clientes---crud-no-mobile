536b72e25ddc1891568d0b5ebbf7935403eb2681
Isabelly Lima <userisabelly09@gmail.com>
2025-10-20 22:58:10 -0300

    barra inicial add

--- app/src/main/java/br/senai/sp/jandira/clientesappdsa/screens/cliente/TelaDicas.kt
+++ app/src/main/java/br/senai/sp/jandira/clientesappdsa/screens/cliente/TelaDicas.kt
@@ -1,111 +1,60 @@
 package br.senai.sp.jandira.clientesappdsa.screens.cliente
 
-import android.content.res.Configuration
-import android.util.Patterns
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import br.senai.sp.jandira.clientesappdsa.model.Cliente
-import br.senai.sp.jandira.clientesappdsa.service.Conexao
-import br.senai.sp.jandira.clientesappdsa.ui.theme.ClientesAppDSATheme
-import br.senai.sp.jandira.clientesappdsa.ui.theme.DicasAppDSATheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.await
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.clientesappdsa.R
 
 @Composable
-fun TelaDicas(padding: PaddingValues, controleNavegacao: NavHostController?) {
-
-    //Criar uma instância da conexão com a API
-    val clienteApi = Conexao().getClienteService()
-
-    Surface (
+fun Dicas(modifier: Modifier){
+    Box(
         modifier = Modifier
-            .padding(padding)
             .fillMaxSize()
+    )
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Red)
     ){
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = "",
+            modifier = Modifier
+                .size(100.dp)
+                .padding(top = 30.dp)
+        )
         Column (
-            modifier =  Modifier
-                .fillMaxSize()
+            modifier = Modifier
+                .fillMaxHeight()
         ){
-            Row (
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(),
-                verticalAlignment =  Alignment.CenterVertically
-            ){
-                Text(
-                    text = "Home",
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Text(
-                    text = "Calendário",
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Text(
-                    text = "Dicas",
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Text(
-                    text = "Rotina",
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Icon(
-                    imageVector =  Icons.Default.Person,
-                    contentDescription = "Person"
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-            }
-            Spacer(modifier =  Modifier
-                .height(16.dp)
+            Text(
+                text = "Home",
+                color = Color.Black,
+                fontSize = 45.sp,
+                fontWeight = FontWeight.Bold
             )
-
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-private fun ClienteFormPreview(){
-    DicasAppDSATheme{
-        TelaDicas(PaddingValues(0.dp), null)
-    }
+private fun TelaDicasPreview(){
+    Dicas(modifier = Modifier)
 }
